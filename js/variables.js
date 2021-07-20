65d5d213c4586da7f7dc58edd33d6c7f51fc5729
Emeka Orji <emekapraiseo@gmail.com>
2021-07-20 12:41:10 +0100

    Tweaked a few things

--- js/variables.js
+++ js/variables.js
@@ -3,8 +3,7 @@ const inputs = document.querySelectorAll("input");
 
 function updateProperty() {
   const suffix = this.dataset.sizing || "";
-  console.log(suffix);
-  document.documentElement.style.setProperty(`--${this.name}`, (this.value + suffix));
+  // console.log(suffix);
+  document.documentElement.style.setProperty(`--${this.name}`, ((this.value / 10) + suffix));
 }
-inputs.forEach(input => input.addEventListener("change", updateProperty));
-inputs.forEach(input => input.addEventListener("mousemove", updateProperty));
+inputs.forEach(input => input.addEventListener("input", updateProperty));
