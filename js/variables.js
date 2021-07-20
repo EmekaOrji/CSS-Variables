e8a8dd805e6adc5846d113e9ccfc086b83fe588e
Emeka Orji <emekapraiseo@gmail.com>
2021-07-20 12:44:13 +0100

    removed comments

--- js/variables.js
+++ js/variables.js
@@ -1,9 +1,6 @@
 const inputs = document.querySelectorAll("input");
-
-
 function updateProperty() {
   const suffix = this.dataset.sizing || "";
-  // console.log(suffix);
   document.documentElement.style.setProperty(`--${this.name}`, ((this.value / 10) + suffix));
 }
 inputs.forEach(input => input.addEventListener("input", updateProperty));
